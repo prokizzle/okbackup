62a346623c019e8ddee98037cb8dda0988317892
Nick Prokesch <nprokesch@gmail.com>
2014-10-27 10:22:01 -0500

    better detection of available fields; better storage file names

--- lib/backup/essay_versions.rb
+++ lib/backup/essay_versions.rb
@@ -6,13 +6,14 @@ module OKBackup
     attr_accessor :auth
 
     def initialize(username, password)
-      @auth = OKProfile::Authentication.new(username: username, password: password, driver: "chrome")
+      @auth = OKBackup::Authentication.new(username: username, password: password, driver: "chrome")
       @auth.login
       @browser = @auth.browser
     end
 
     def backup_profile(path)
       essays = []
+      essay_names = {:"0" => "self_summary", :"1" => "what_im_doing_with_my_life", :"2" => "im_really_good_at", :"3" => "first_thing_people_notice_about_me", :"4" =>"favorites", :"5" => "six_things", :"6" => "think_about", :"7" => "friday_night", :"9" => "message_me_if"}
       b = @auth.browser
       b.goto "http://okcupid.com/profile"
       begin
@@ -21,12 +22,21 @@ module OKBackup
         b.window(:url => 'http://www.okcupid.com/profile').use
       end
       b.links(class: "essay_title").each{|m| m.click}
-      b.textareas.each{|c| essays.push c.value}
-      essays.uniq.each_with_index do |essay, index|
-        unless essay.empty?
-        File.open("#{path}/essay_#{index}.txt", 'w') { |file| file.write(essay) }
+
+      essay_names.each do |index, name|
+        begin
+          essay = b.textarea(id: "edit_#{index}").value
+          File.open("#{path}/#{name}.txt", 'w') { |file| file.write(essay) }
+        rescue
+        end
       end
-      end
+
+      # b.textareas.each{|c| essays.push c.value}
+      # essays.uniq.each_with_index do |essay, index|
+      #   # unless essay.empty?
+      #     File.open("#{path}/essay_#{index}.txt", 'w') { |file| file.write(essay) }
+      #   # end
+      # end
       @auth.browser.close
     end
 
